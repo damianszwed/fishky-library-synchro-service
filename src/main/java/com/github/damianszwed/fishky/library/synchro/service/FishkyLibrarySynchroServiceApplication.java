0087cf6f6d59c19eb727fa463574d1db95ccbcd7
Damian Szwed <damian.szwed@10g.pl>
2021-10-14 11:46:18 +0200

    Spring init.

---
+++ src/main/java/com/github/damianszwed/fishky/library/synchro/service/FishkyLibrarySynchroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.damianszwed.fishky.library.synchro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishkyLibrarySynchroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishkyLibrarySynchroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/damianszwed/fishky/library/synchro/service/FishkyLibrarySynchroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.damianszwed.fishky.library.synchro.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishkyLibrarySynchroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
