05f950ffd65ccfc3192fde63029d1ef012ef1a69
Damian Szwed <damian.szwed@10g.pl>
2021-10-15 15:37:17 +0200

    scheduler rate prop

--- src/main/java/com/github/damianszwed/fishky/library/synchro/service/FishkyLibrarySynchroServiceApplication.java
+++ src/main/java/com/github/damianszwed/fishky/library/synchro/service/FishkyLibrarySynchroServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.damianszwed.fishky.library.synchro.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FishkyLibrarySynchroServiceApplication {
 
 	public static void main(String[] args) {
