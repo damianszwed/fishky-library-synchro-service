9a574e1b482e6e2ad0eea61d27d7c06d8badd21c
Damian Szwed <damian.szwed@10g.pl>
2023-03-29 17:08:02 +0200

    spring active profile by env var

---
+++ src/main/java/com/github/damianszwed/fishky/library/synchro/service/configuration/ElasticSearchSynchroConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.damianszwed.fishky.library.synchro.service.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("elasticsearch")
+public class ElasticSearchSynchroConfiguration {
+
+}
